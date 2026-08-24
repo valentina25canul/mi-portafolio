9fa68bdcfe7785d95d4c453ca1b7130941573840
valentina25canul <valentinacanul25@gmail.com>
2026-08-24 16:58:31 -0600

    Añade nuevo icono de portafolio

--- resources/views/p4.blade.php
+++ resources/views/p4.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('icon.ico') }}">
     <title>App recertario</title>
     @vite('resources/css/app.css')
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('icon.ico') }}">
     <title>Mi Portafolio</title>
     @vite('resources/css/app.css')
 </head>
